cc2fb6a86e92ac177d38a1fb3f4a593799dff563
Lionel Townsend <townsendlionel@gmail.com>
2021-03-01 21:36:00 -0600

    added to note route

--- Develop/routes/apiRoutes/noteRoutes.js
+++ Develop/routes/apiRoutes/noteRoutes.js
@@ -1,16 +1,22 @@
 const router = require('express').Router();
-
+const db = require('../../db/db.json');
 
 router.get('/notes', (req, res) => {
+    console.log(db.notes);
     console.log('api get notes');
+    res.json(db.notes);
 });
 
 router.post('/notes', (req, res) => {
     console.log('api post note');
+    console.log(JSON.stringify(req.body));
+
+    // add to db.json
 });
 
-router.delete('/notes:id', (req, res) => {
+router.delete('/notes', (req, res) => {
     console.log('api delete note');
+    console.log(JSON.stringify(req.params));
 });
 
 
