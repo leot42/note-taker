7e5a637ad8b57d6952c87fa40df02dc95670350d
Lionel Townsend <townsendlionel@gmail.com>
2021-03-02 19:46:09 -0600

    sketched out db manager

--- Develop/lib/dbManager.js
+++ Develop/lib/dbManager.js
@@ -1,7 +1,24 @@
 const dataStore = require('../db/db.json');
+const path = require('path');
+const dataLocation = path
 const noteList = dataStore.notes;
 //const fs = require('fs').
 // const nextNewId = dataStore.nextNewId;
+
+/*
+    get data from file
+    keep data in memory
+        onGet 
+            return data from memory
+        onPost
+            get nextNewId
+            add to nextNewId as id number
+            add modified note data to in memory db representation
+            append as new line to a log file
+            every 10 adds to the log add pending changes to the main storage file -fs will rewrite whole file
+        onDelete alter variable in memory
+            then trigger file save every so often
+*/
 
 function addRecord(note) {
     let nextNewId = dataStore.nextNewId;
