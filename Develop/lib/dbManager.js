5a3b132956f78feb7a4d2fb45a062ab50a7062f4
Lionel Townsend <townsendlionel@gmail.com>
2021-03-02 19:24:24 -0600

    started work on new db manager

---
+++ Develop/lib/dbManager.js
@@ -0,0 +1,15 @@
+const dataStore = require('../db/db.json');
+const noteList = dataStore.notes;
+//const fs = require('fs').
+// const nextNewId = dataStore.nextNewId;
+
+function addRecord(note) {
+    let nextNewId = dataStore.nextNewId;
+    f
+}
+
+function deleteRecord(id);
+
+function getAllRecords() {
+
+}
--- Develop/routes/apiRoutes/noteRoutes.js
+++ Develop/routes/apiRoutes/noteRoutes.js
@@ -1,23 +1,25 @@
 const router = require('express').Router();
 const db = require('../../db/db.json');
 
+router.delete('/notes/:id', (req, res) => {
+    console.log(`api delete notes ${JSON.stringify(req.params)}`);
+
+   console.log(JSON.stringify(req.params));
+   res.send(200);
+});
+
 router.get('/notes', (req, res) => {
-    console.log(db.notes);
-    console.log('api get notes');
+ //   console.log(`api get notes ${db.notes}`);
     res.json(db.notes);
 });
 
 router.post('/notes', (req, res) => {
-    console.log('api post note');
+    console.log(`api post notes ${db.notes}`);
     console.log(JSON.stringify(req.body));
 
     // add to db.json
 });
 
-router.delete('/notes', (req, res) => {
-    console.log('api delete note');
-    console.log(JSON.stringify(req.params));
-});
 
 
 module.exports = router;
