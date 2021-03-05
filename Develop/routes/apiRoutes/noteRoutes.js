7074613c9280dd977e6d30787d7869c3251e427d
Lionel Townsend <townsendlionel@gmail.com>
2021-03-05 13:20:02 -0600

    fixed dataStore error

--- Develop/routes/apiRoutes/noteRoutes.js
+++ Develop/routes/apiRoutes/noteRoutes.js
@@ -1,30 +1,32 @@
 const fs = require('fs');
-const path = require('path'); 
+const path = require('path');
 const router = require('express').Router();
-const db = require('../../db/db.json');
+const dataStore = require('../../db/db.json');
 const dataLocation = path.join(__dirname, '../../db/db.json');
 
 router.delete('/notes/:id', (req, res) => {
     console.log(`api delete notes ${JSON.stringify(req.params)}`);
 
-   console.log(JSON.stringify(req.params));
-   res.send(200);
+    console.log(JSON.stringify(req.params));
+    res.send(200);
 });
 
 router.get('/notes', (req, res) => {
- //   console.log(`api get notes ${db.notes}`);
-    res.json(db.notes);
+    //   console.log(`api get notes ${db.notes}`);
+    res.json(dataStore.db.notes);
 });
 
 router.post('/notes', (req, res) => {
-    console.log(`api post notes ${db.notes}`);
-    console.log(JSON.stringify(req.body));
-    db.notes.push(req.body);
+  //  console.log(`api post notes ${dataStore.db.notes}`);
 
+    req.body.id = dataStore.db.nextNewId;
+    dataStore.db.nextNewId++;
+    console.log(JSON.stringify(req.body));
+    dataStore.db.notes.push(req.body);
     fs.writeFileSync(
         dataLocation,
-        JSON.stringify({ db }, null, 2));
-    
+        JSON.stringify({ dataStore }, null, 2));
+    res.json(dataStore.db.notes);
 });
 
 
