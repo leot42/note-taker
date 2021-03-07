f5a6cc729a2de323c5d0275abb4fa04051bdecce
Lionel Townsend <townsendlionel@gmail.com>
2021-03-07 10:58:38 -0600

    removed some comments

--- Develop/routes/apiRoutes/noteRoutes.js
+++ Develop/routes/apiRoutes/noteRoutes.js
@@ -7,16 +7,14 @@ const dataLocation = path.join(__dirname, '../../db/db.json');
 router.delete('/notes/:id', (req, res) => {
     console.log(`api delete notes where id = ${JSON.stringify(req.params.id)}`);
     let noteArr = dataStore.db.notes;
-    // console.log(`noteArr intialized to = ${JSON.stringify(noteArr)}`);
-    // filter out id of x 
+  
     let tempNoteArrayWithIdXRemoved = noteArr.filter((note) => {
    
         if (note.id != Number(req.params.id)) return note;
      });
 
      dataStore.db.notes = tempNoteArrayWithIdXRemoved;
-            // console.log(` map note check ${JSON.stringify(note)}`);
-    // } )
+   
     console.log(` filtered id check ${JSON.stringify(dataStore)}`);
 
     saveBackToJsonFile();
@@ -25,13 +23,11 @@ router.delete('/notes/:id', (req, res) => {
 });
 
 router.get('/notes', (req, res) => {
-    //  console.log(`api get notes ${JSON.stringify(dataStore.db.notes)}`);
     res.json(dataStore.db.notes);
 });
 
 router.post('/notes', (req, res) => {
-//    console.log(`api post notes ${dataStore.db.notes}`);
-    // console.log(` post ${JSON.stringify(dataStore, null, 2)}`);
+le.log(` post ${JSON.stringify(dataStore, null, 2)}`);
     req.body.id = dataStore.db.nextNewId;
 
 
